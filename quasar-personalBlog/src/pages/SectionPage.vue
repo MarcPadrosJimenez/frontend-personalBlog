8f3299798175308c0a85972a520b02a9fac53cb9
Marc Padrós <marcpj14@gmail.com>
2024-12-01 21:09:44 +0100

    pasar parametro seccion de routes a SectionPage

--- quasar-personalBlog/src/pages/SectionPage.vue
+++ quasar-personalBlog/src/pages/SectionPage.vue
@@ -1,23 +1,57 @@
 <template>
   <div class="q-pa-md q-gutter-sm">
-    <q-btn color="white" @click="handleClick" text-color="black" label="Create" />
+    <h2>{{ $route.params.section }}</h2>
+    <q-btn color="white" @click="handleClickCreate" text-color="black" label="Create" />
+    <q-btn color="white" v-if="showTextEditor" @click="handleClickSave" text-color="black" label="Save" />
   </div>
-  <QuillEditor theme="snow" />
+  <QuillEditor v-if="showTextEditor" theme="snow" />
+  <ul v-if="items.length > 0">
+    <li v-for="item in items" :key="item.id"><QuillEditor content={{item.name}} theme="snow" /></li>
+  </ul>
+  <p v-else>Come on, start posting!</p>
 </template>
 
 <script>
   import { QuillEditor } from '@vueup/vue-quill';
   import '@vueup/vue-quill/dist/vue-quill.snow.css';
+  import axios from 'axios';
 
   export default {
     components: {
       QuillEditor
     },
+    data() {
+      return {
+        items: []
+      }
+    },
+    mounted() {
+      // ciclo de vida mounted, cuando se carga la página
+      this.fetchBlogPosts();
+      const section = this.$route.params.section;
+    },
     methods: {
-      handleClick() {
-        console.log('¡Se hizo clic en el botón!');
-        // Aquí puedes agregar tu lógica personalizada
-        // Por ejemplo, mostrar un diálogo, enviar datos, etc.
+      fetchBlogPosts() {
+        axios.get('https://localhost:8000/' + section + '/posts')
+          .then(response => {
+            this.items = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          })
+      },
+      handleClickCreate() {
+        this.showTextEditor=true;
+      },
+      handleClickSave(){
+        this.showTextEditor=false;
+        axios.post('/api/enviarDatos', this.datos)
+        .then(response => {
+            // Manejar la respuesta del servidor
+        })
+        .catch(error => {
+            // Manejar errores
+        });
       }
     }
   }
--- quasar-personalBlog/src/router/routes.js
+++ quasar-personalBlog/src/router/routes.js
@@ -4,10 +4,7 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [
       { path: "", component: () => import("pages/IndexPage.vue") },
-      { path: "Business/", name: 'business', component: () => import("pages/SectionPage.vue") },
-      { path: "Tennis/", name: 'tennis', component: () => import("pages/SectionPage.vue")},
-      { path: "Gym/", name: 'gym', component: () => import("pages/SectionPage.vue") },
-      { path: "Trips/", name: 'trips', component: () => import("pages/SectionPage.vue")}
+      { path: ":section", name: 'section', component: () => import("pages/SectionPage.vue") },
     ],
   },
 
